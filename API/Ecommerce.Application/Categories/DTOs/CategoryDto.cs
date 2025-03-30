0a933cfdfe42d56558c518f69c2e48877e06c65c
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-03-30 21:15:02 +0700

    chore

--- API/Ecommerce.Application/Categories/DTOs/CategoryDto.cs
+++ API/Ecommerce.Application/Categories/DTOs/CategoryDto.cs
@@ -5,8 +5,8 @@ namespace Ecommerce.Application.Categories.DTOs;
 public class CategoryDto
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
     public int? ParentId { get; set; }
-    public string ParentName { get; set; }
-    public List<CategoryDto> Children { get; set; }
+    public string? ParentName { get; set; }
+    public List<CategoryDto> Children { get; set; } = null!;
 }
--- API/Ecommerce.Application/Categories/DTOs/CreateCategoryDto.cs
+++ API/Ecommerce.Application/Categories/DTOs/CreateCategoryDto.cs
@@ -2,6 +2,6 @@ namespace Ecommerce.Application.Categories.DTOs;
 
 public class CreateCategoryDto
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
     public int? ParentId { get; set; }
 }
