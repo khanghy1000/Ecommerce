721642eec6aac89c90199da46d3a5e48cbcb1f7b
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-05-05 18:43:35 +0700

    fix(client): wrong Notification component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import { MantineProvider, Notification } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
 import { StrictMode } from 'react';
@@ -6,17 +6,17 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Notifications } from '@mantine/notifications';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider>
-      <Notification>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
-      </Notification>
+      <Notifications />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </MantineProvider>
   </StrictMode>
 );
