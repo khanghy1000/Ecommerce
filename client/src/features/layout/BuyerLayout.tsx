5dca97c56fe5fed1de2a6cb8147b1f30c2356b8f
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-05-06 10:53:09 +0700

    feat(client): scroll to top when route changes

--- client/src/features/layout/BuyerLayout.tsx
+++ client/src/features/layout/BuyerLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router';
 import { BuyerNavbar } from './BuyerNavbar';
+import ScrollToTop from '../../lib/components/ScrollToTop';
 
 export function BuyerLayout() {
   return (
     <>
+      <ScrollToTop />
       <BuyerNavbar />
       <Outlet />
     </>
