d4f2d364180ebd8a1f7f96db20aa2b854c9c9992
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2025-05-20 16:48:56 +0700

    fix: add missing product photos to the get order by ID API endpoint

--- API/Ecommerce.Application/SalesOrders/Queries/GetSalesOrderById.cs
+++ API/Ecommerce.Application/SalesOrders/Queries/GetSalesOrderById.cs
@@ -1,11 +1,9 @@
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Ecommerce.Application.Core;
-using Ecommerce.Application.Interfaces;
 using Ecommerce.Application.SalesOrders.DTOs;
-using Ecommerce.Domain;
 using Ecommerce.Persistence;
 using MediatR;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Application.SalesOrders.Queries;
@@ -26,17 +24,16 @@ public static class GetSalesOrderById
         )
         {
             var salesOrder = await dbContext
-                .SalesOrders.Include(o => o.OrderProducts)
-                .ThenInclude(op => op.Product)
-                .FirstOrDefaultAsync(o => o.Id == request.Id, cancellationToken);
+                .SalesOrders.Where(o => o.Id == request.Id)
+                .ProjectTo<SalesOrderResponseDto>(mapper.ConfigurationProvider)
+                .FirstOrDefaultAsync(cancellationToken);
 
             if (salesOrder == null)
             {
                 return Result<SalesOrderResponseDto>.Failure("Sales order not found", 404);
             }
 
-            var salesOrderDto = mapper.Map<SalesOrderResponseDto>(salesOrder);
-            return Result<SalesOrderResponseDto>.Success(salesOrderDto);
+            return Result<SalesOrderResponseDto>.Success(salesOrder);
         }
     }
 }
